dcaac9d0e16c7d334e7105abfda3ef8b3a58a4ec
Ken <johnkgervacio@gmail.com>
2022-03-17 09:38:44 +0800

    starter finished

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./public/*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        primary: {
+          100: '#f5f6fa',
+          200: '#6dd5ed'
+        },
+        secondary__1: {
+          100:'#f5f6fa',
+          200: '#dcdde1',
+        }
+
+      }
+    },
+  },
+  plugins: [],
+}
